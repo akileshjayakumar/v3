7bb6af839c4ef83938bfaee00f9220f75edd64f5
Akilesh Jayakumar <akileshjayakumar88@gmail.com>
2025-08-07 16:36:45 +0800

    Enhance layout by adding favicon and improving code formatting

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,22 +1,25 @@
-import type { Metadata } from 'next'
-import { Inter } from 'next/font/google'
-import './globals.css'
-import { ThemeProvider } from '@/components/theme-provider'
+import type { Metadata } from "next";
+import { Inter } from "next/font/google";
+import "./globals.css";
+import { ThemeProvider } from "@/components/theme-provider";
 
-const inter = Inter({ subsets: ['latin'] })
+const inter = Inter({ subsets: ["latin"] });
 
 export const metadata: Metadata = {
-  title: 'Akilesh Jayakumar',
-  description: 'Portfolio Website of Akilesh Jayakumar',
-}
+  title: "Akilesh Jayakumar",
+  description: "Portfolio Website of Akilesh Jayakumar",
+};
 
 export default function RootLayout({
   children,
 }: {
-  children: React.ReactNode
+  children: React.ReactNode;
 }) {
   return (
     <html lang="en" suppressHydrationWarning>
+      <head>
+        <link rel="icon" href="/favicon.ico" />
+      </head>
       <body className={inter.className}>
         <ThemeProvider
           attribute="class"
@@ -28,5 +31,5 @@ export default function RootLayout({
         </ThemeProvider>
       </body>
     </html>
-  )
+  );
 }
